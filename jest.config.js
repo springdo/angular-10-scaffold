26ee8df9d1cf06ed40fcbf170fb49ac12de68a94
donal <donalspring@gmail.com>
2020-07-13 18:31:11 +0100

    🃏 ADD - Jest for super testing 🃏

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+// https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['src'],
+  collectCoverage: true,
+  coverageReporters: ['html'],
+  coverageDirectory: 'reports/coverage',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/src/app/$1',
+    '@env': '<rootDir>/src/environments/environment'
+  },
+  // Do not ignore librairies such as ionic, ionic-native or bootstrap to transform them during unit testing.
+  transformIgnorePatterns: ['node_modules/(?!(jest-test|@ng-bootstrap))']
+};
