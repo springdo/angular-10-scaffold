9f4a49e7169629ba8af6afcc822f4e5a29d470c2
donal <donalspring@gmail.com>
2020-07-13 16:35:58 +0100

    💄 add prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lxp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'learning-experience-platform';
